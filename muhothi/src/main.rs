ad6eccb9e73ad645e6b0c292574918f9b0abc14f
SitrusMint <chocomnt242@gmail.com>
2020-09-26 17:53:21 +0900

    バグ修正と[]バグの発見

--- muhothi/src/main.rs
+++ muhothi/src/main.rs
@@ -4,13 +4,16 @@ use lib::{execute_bf};
 use std::env;
 use std::fs;
 
-fn main() -> Result<(), ()>{
+fn main() { 
     let args: Vec<String> = env::args().collect();
-    let content = fs::read_to_string(&args[1]).expect("file couldn't be open");
+    if args.len() != 2 {
+        println!("incorrect argument")
+    }
 
-    if let Ok(()) = execute_bf(&content) {
-        Ok(())
-    } else {
-        Err(())
+    let content = fs::read_to_string(&args[1]).unwrap();
+
+    match execute_bf(&content) {
+        Ok(_) => {},
+        Err(e) => println!("{}", e),
     }
 }
